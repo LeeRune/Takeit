4f2996067fa30e4743943a679647fad16592766e
LeeRune <75962180+LeeRune@users.noreply.github.com>
2021-03-09 21:24:04 +0800

    Add files via upload

---
+++ TakeItLogin/TakeItLogin/Rune/Comment.swift
@@ -0,0 +1,14 @@
+//
+//  Comment.swift
+//  Takeit_iOS
+//
+//  Created by 李易潤 on 2021/2/24.
+//
+
+import Foundation
+
+struct Comment {
+    var comment: String
+    var imageName: String
+    var star: Int
+}
